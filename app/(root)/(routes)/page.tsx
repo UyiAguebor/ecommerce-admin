48a32c53400859c766029cff6c7f21d232ffaba8
Uyi Aguebor <uxa287@student.bham.ac.uk>
2023-10-01 10:37:19 +0100

    final

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { userStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+const SetupPage = () => {
+  const onOpen = userStoreModal((state) => state.onOpen);
+  const isOpen = userStoreModal((state) => state.isOpen);
+  
+  useEffect(() => {
+    if(!isOpen) {
+      onOpen();
+    }
+  },[isOpen,onOpen]);
+
+    return null;
+  }
+  
+  export default SetupPage;
